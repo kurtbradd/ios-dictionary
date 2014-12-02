9a29a6e7a00f701eb294effd627a9d041f3a2dd8
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-02 17:16:25 -0500

    init settings controller

---
+++ dictionary/KBSettingsController.h
@@ -0,0 +1,13 @@
+//
+//  KBSettingsController.h
+//  dictionary
+//
+//  Created by Kurt DaCosta on 2014-12-02.
+//  Copyright (c) 2014 kurtbardd. All rights reserved.
+//
+
+#import "KBViewController.h"
+
+@interface KBSettingsController : KBViewController
+
+@end
---
+++ dictionary/KBSettingsController.m
@@ -0,0 +1,38 @@
+//
+//  KBSettingsController.m
+//  dictionary
+//
+//  Created by Kurt DaCosta on 2014-12-02.
+//  Copyright (c) 2014 kurtbardd. All rights reserved.
+//
+
+#import "KBSettingsController.h"
+
+@interface KBSettingsController ()
+
+@end
+
+@implementation KBSettingsController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
