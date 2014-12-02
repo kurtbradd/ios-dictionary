328ceecb129fd56507f6481ac49f9f27a3925158
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-02 15:04:42 -0500

    Initial Commit

---
+++ dictionary/KBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KBAppDelegate.h
+//  dictionary
+//
+//  Created by Kurt DaCosta on 2014-12-02.
+//  Copyright (c) 2014 kurtbardd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dictionary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dictionary
+//
+//  Created by Kurt DaCosta on 2014-12-02.
+//  Copyright (c) 2014 kurtbardd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KBAppDelegate class]));
+    }
+}
