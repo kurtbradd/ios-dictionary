13e3a19ca4745e3ab87eb586b4f080382e31254a
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-03 14:09:21 -0500

    removed default scaffolding

--- dictionary/KBViewController.m
+++ dictionary/KBViewController.m
@@ -23,16 +23,4 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-
 @end
