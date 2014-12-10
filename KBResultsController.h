078395a993dcba6fecf3fc3bea429deef75ed5f5
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-10 02:17:56 -0500

    search results vc starting implementation

---
+++ KBResultsController.h
@@ -0,0 +1,15 @@
+//
+//  KBResultsController.h
+//  dictionary
+//
+//  Created by Kurt DaCosta on 2014-12-09.
+//  Copyright (c) 2014 kurtbardd. All rights reserved.
+//
+
+#import "KBViewController.h"
+
+@interface KBResultsController : KBViewController <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+
+- (instancetype)initSearchWord:(NSString *)word withResults:(NSDictionary *)results;
+
+@end
