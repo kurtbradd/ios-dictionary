315a9ddbdb703ac4d74cb3ab1e88cf3a034cbf67
Kurt DaCosta <kurtbradd@gmail.com>
2014-12-10 02:09:13 -0500

    save user defaults on app exit

--- dictionary/KBAppDelegate.m
+++ dictionary/KBAppDelegate.m
@@ -31,12 +31,14 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+    [[NSUserDefaults standardUserDefaults] synchronize];
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
@@ -47,6 +49,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    [[NSUserDefaults standardUserDefaults] synchronize];
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
